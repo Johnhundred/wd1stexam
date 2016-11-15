529bde38980b3769995369d688296621f619d184
Ralf Patrik Blaga <ralf.patric@yahoo.com>
2016-11-15 14:49:39 +0100

    added notif blck

--- js/main.js
+++ js/main.js
@@ -137,8 +137,20 @@ notifyMe();*/
 
 /* ----- ----- This is using promises ----- ----- */
 
+
 Notification.requestPermission().then(function() {
-    var notification4 = new Notification ("Works with promises as well");
-    $("#ping")[0].play();
+        sNotification = Notification.permission;
+
+        if (sNotification === "granted") {
+            //console.log(Notification.permission);
+            var notification4 = new Notification ("Works with promises as well");
+            $("#ping")[0].play();
+        } else {
+            swal("In order to fully experience our website please enable Desktop Notifications!");
+        }
+
+
 });
+
+
 
